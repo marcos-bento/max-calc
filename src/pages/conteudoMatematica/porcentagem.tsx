c08c4d3431f3c6829131a5583f9f3f9a6225aa69
Marcos Bento <marcos.x.marcos@hotmail.com>
2024-01-31 16:31:24 -0300

    Finalização da página de porcentagem

--- src/pages/conteudoMatematica/porcentagem.tsx
+++ src/pages/conteudoMatematica/porcentagem.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, useState } from "react";
+import React, { ChangeEvent, useEffect, useState } from "react";
 import Botao from "../../components/botao/botao";
 
 interface InputProps {
@@ -20,6 +20,14 @@ export default function Porcentagem() {
     const [valorFinal3, setValorFinal3] = useState<number>();
     const [resposta3, setResposta3] = useState<number>();
 
+    useEffect(() => {
+        limparResolucao();
+    }, [tipoDeEntrada]);
+
+    const limparResolucao = () =>{
+        setResolucao(undefined);
+    };
+
     const calculaResposta = (tipo: string) => {
         let calculo: React.ReactNode;
         switch (tipo) {
@@ -30,13 +38,13 @@ export default function Porcentagem() {
                     calculo = (
                         <>
                             <p>{valor1} / por 100 = {valor1 / 100}</p>
-                            <p>{valor1 / 100} * {percentual1} = {((valor1 / 100) * percentual1).toFixed(2).replace(".00", "")}</p>
+                            <p>{valor1 / 100} x {percentual1} = {((valor1 / 100) * percentual1).toFixed(2).replace(".00", "")}</p>
                             <p>{percentual1}% de {valor1} é igual a {((valor1 / 100) * percentual1).toFixed(2).replace(".00", "")}!</p>
                         </>
                     );
                 } else {
                     alert("Preencha os campos corretamente!");
-                }
+                };
                 break;
             case "B":
                 // Calculo simples acréscimo ou desconto de X% de um determinado valor
@@ -46,14 +54,14 @@ export default function Porcentagem() {
                     calculo = (
                         <>
                             <p>{valor2} / 100 = {valor2 / 100}</p>
-                            <p>{valor2 / 100} * {percentual2} = {porcento}</p>
+                            <p>{valor2 / 100} x {percentual2} = {porcento}</p>
                             <p>{valor2} {condicional2 === 1 ? "+" : "-"} {porcento}</p>
                             <p>{percentual2}% de {condicional2 === 1 ? "acréscimo" : "desconto"} sobre {valor2} é igual a {condicional2 === 1 ? porcento + valor2 : valor2 - porcento}!</p>
                         </>
                     );
                 } else {
                     alert("Preencha os campos corretamente!");
-                }
+                };
                 break;
                 case "C":
                 // Calculo simples descobrir o diferencial de um percentual
@@ -64,13 +72,13 @@ export default function Porcentagem() {
                         <>
                             <p>{valorFinal3} - {valorInicial3} = {valorFinal3 - valorInicial3}</p>
                             <p>{valorFinal3 - valorInicial3} / {valorInicial3} = {porcento}</p>
-                            <p>{porcento} * 100 = {porcento * 100}</p>
+                            <p>{porcento} x 100 = {porcento * 100}</p>
                             <p>A diferença entre {valorInicial3} e {valorFinal3} é de {porcento*100}%!</p>
                         </>
                     );
                 } else {
                     alert("Preencha os campos corretamente!");
-                }
+                };
                 break;
             default:
                 break;
@@ -137,7 +145,7 @@ export default function Porcentagem() {
                 </select>
                 <p>de</p>
                 <input type="number" min={1} value={percentual2} onChange={(event) => handleValueChange(event, { setValue: setPercentual2 })} placeholder="x %" required />
-                <p>sobre</p>
+                <p>% sobre</p>
                 <input type="number" min={1} value={valor2} onChange={(event) => handleValueChange(event, { setValue: setValor2 })} placeholder="valor" required />
                 <p>=</p>
                 <input type="number" min={1} value={resposta2} onChange={(event) => handleValueChange(event, { setValue: setResposta2 })} placeholder="resultado" disabled />
@@ -160,7 +168,15 @@ export default function Porcentagem() {
             }
             <div className="conteudo__resolucao">{resolucao}</div>
             <hr></hr>
-
-        </>
+            <div className="conteudo__material">
+                <p className="conteudo__material__titulo">Sobre</p>
+                <p>Porcentagem, representada pelo símbolo %, é a divisão de um número qualquer por 100. A expressão 25%, por exemplo, significa que 25 partes de um todo foram divididas em 100 partes.</p>
+                <p>Há três formas de representar uma porcentagem: forma percentual, forma fracionária e forma decimal. O cálculo do valor representado por uma porcentagem geralmente é feito a partir de uma multiplicação de frações ou de números decimais, por isso o domínio das quatro operações é fundamental para a compreensão de como calcular corretamente uma porcentagem.</p>
+                <p>Forma Percentual: A representação na forma percentual ocorre quando o número é seguido do símbolo % (por cento). Exemplo: 5%,  0.1%,  150%.</p>
+                <p>Forma Fracionária: Para realização de cálculos, uma das formas possíveis de representação de uma porcentagem é a forma fracionária, que pode ser uma fração irredutível ou uma simples fração sobre o número 100. Ex:</p>
+                <img src="https://static.mundoeducacao.uol.com.br/mundoeducacao/2020/06/1-fracao.jpg" alt="Exemplo de percentual sendo transformado em fração" />
+                <p>Forma Decimal: A forma decimal é uma possibilidade de representação também. Para encontrá-la, é necessária a realização da divisão. Ex: A forma decimal de 25% é obtida pela divisão de 25 : 100 = 0,25.</p>
+            </div>
+        </> 
     )
 }
